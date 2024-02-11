19bd248128207fe58ff2f19d2b5867917d7514bc
Priyal <minnipriyal12@gmail.com>
2024-02-11 21:17:36 +0530

    tablemultiplication

---
+++ SampleProject/src/table.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Eneter the any number");
+        int num = input.nextInt();
+        multiplicationTable(num);
+
+    }
+    public static void multiplicationTable(int num){
+        int i=1;
+        while(i<=10){
+            System.out.println(num +"X"+ i +"="+(num*i) );
+            i++;
+        }
+    }
+}
