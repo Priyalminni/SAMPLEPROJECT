a40f2c36c2570fa01ddfc745c6ca5c98b0559be6
Priyal <minnipriyal12@gmail.com>
2024-02-24 17:23:20 +0530

    find min_value

---
+++ SampleProject/src/array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4,5,6};
+        int min=Integer.MAX_VALUE;
+        for(int num:nums){
+            if(min>num){
+                min=num;
+            }
+        }
+        System.out.println("min is "+min);
+    }
+}
