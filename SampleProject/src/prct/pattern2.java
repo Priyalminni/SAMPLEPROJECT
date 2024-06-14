813a0317fc4962490d4fab520512023edc597342
Priyal <minnipriyal12@gmail.com>
2024-06-14 15:07:52 +0530

    decrementpattern

--- SampleProject/src/prct/pattern2.java
+++ SampleProject/src/prct/pattern2.java
@@ -2,11 +2,20 @@ package prct;
 
 public class pattern2 {
     public static void main(String[] args) {
+        pattern1(4);
         pattern2(5);
     }
     static void pattern2(int n){
         for (int row=0;row<n;row++){
             for (int col=5;col>row;col--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern1(int n){
+        for (int row=0;row<=n;row++){
+            for (int col=row;col<=n;col++){
                 System.out.print("* ");
             }
             System.out.println();
