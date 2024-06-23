14233adbe0354c9f4b355f8ee9219385b25c3805
Priyal <minnipriyal12@gmail.com>
2024-06-23 13:25:35 +0530

    noargs constuctor_practis

---
+++ SampleProject/src/constructor.java
@@ -0,0 +1,11 @@
+public class constructor {
+
+    constructor(){
+        System.out.println("no arg constructor");
+    }
+    public static void main(String[] args) {
+        constructor obj=new constructor();
+    
+    }
+
+}
