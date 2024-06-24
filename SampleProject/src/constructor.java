b52742ba6c606673b19c295f244657ee1889c685
Priyal <minnipriyal12@gmail.com>
2024-06-24 10:12:37 +0530

    inheritance pratics

--- SampleProject/src/constructor.java
+++ SampleProject/src/constructor.java
@@ -1,11 +1,10 @@
 public class constructor {
-
+//no args constructor
     constructor(){
         System.out.println("no arg constructor");
     }
     public static void main(String[] args) {
         constructor obj=new constructor();
-    
     }
-
 }
+//no args construtor
